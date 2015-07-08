438a7411d2677bdc677f86ac4596c207de1343c8
svat30 <shikhar.sameer@gmail.com>
2015-07-08 23:51:39 +0530

    first commit

--- Tables Made Easy/ViewController.swift
+++ Tables Made Easy/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBAction func slider(sender: AnyObject) {
+    
+        table.reloadData()
+        
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 20
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let number = Int(sliderValue.value)
+        
+        let multiple = indexPath.row + 1
+        
+        cell.textLabel?.text = " \(number) X \(multiple) = \(number * multiple) "
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
