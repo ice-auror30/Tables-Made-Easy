0a268655869997b7c36449b512a8d44ed30f8597
svat30 <shikhar.sameer@gmail.com>
2015-07-08 22:55:08 +0530

    Initial Commit

---
+++ Tables Made EasyTests/Tables_Made_EasyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tables_Made_EasyTests.swift
+//  Tables Made EasyTests
+//
+//  Created by Shikhar Vats on 7/8/15.
+//  Copyright (c) 2015 IceAuror. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tables_Made_EasyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
